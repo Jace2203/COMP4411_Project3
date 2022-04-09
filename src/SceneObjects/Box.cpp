95711da0601aaac7e4df2808025742d8f9ee5137
Jace2203 <thomasw2203@gmail.com>
2022-04-09 19:56:25 +0800

    Fix Box intersect

--- src/SceneObjects/Box.cpp
+++ src/SceneObjects/Box.cpp
@@ -12,78 +12,44 @@ bool Box::intersectLocal( const ray& r, isect& i ) const
 	// YOUR CODE HERE:
     // Add box intersection code here.
 	// it currently ignores all boxes and just returns false.
-
-	// Smit's method
+	double tMin, tMax;
+	BoundingBox bound = ComputeLocalBoundingBox();
+	if (!bound.intersect(r, tMin, tMax))
+		return false;
+	
+	i.t = tMin;
+	i.obj = this;
+	
 	vec3f p = r.getPosition();
 	vec3f d = r.getDirection();
-
-	double tNear, tFar, tyNear, tyFar, tzNear, tzFar;
-	int axis = 0;
-
-	// X
-	if (d[0] >= 0) {
-		tNear = (-0.5 - p[0]) / d[0];
-		tFar =  ( 0.5 - p[0]) / d[0];
-	}
-	else
-	{
-		tNear = ( 0.5 - p[0]) / d[0];
-		tFar =  (-0.5 - p[0]) / d[0];
-	}
-
-	// Y
-	if (d[1] >= 0)
-	{
-		tyNear = (-0.5 - p[1]) / d[1];
-		tyFar =  ( 0.5 - p[1]) / d[1];
-	}
-	else
-	{
-		tyNear = ( 0.5 - p[1]) / d[1];
-		tyFar =  (-0.5 - p[1]) / d[1];
-	}
-
-	if ((tNear > tyFar) || (tyNear > tFar))
-		return false;
-	if (tyNear > tNear)
-	{
-		tNear = tyNear;
-		axis = 1;
-	}
-	if (tyFar < tFar)
-		tFar = tyFar;
-
-	// Z
-	if (d[2] >= 0)
-	{
-		tzNear = (-0.5 - p[2]) / d[2];
-		tzFar =  ( 0.5 - p[2]) / d[2];
-	}
-	else
-	{
-		tzNear = ( 0.5 - p[2]) / d[2];
-		tzFar =  (-0.5 - p[2]) / d[2];
-	}
 	
-	if ((tNear > tzFar) || (tzNear > tFar))
-		return false;
-	if (tzNear > tNear)
-	{
-		tNear = tzNear;
-		axis = 2;
-	}
-	if (tzFar < tFar)
-		tFar = tzFar;
+	double tyMin, tzMin;
+	int axis = 0;
+	
+	if (d[1] > 0.0)
+		tyMin = (-0.5 - p[1]) / d[1];
+	else
+		tyMin = ( 0.5 - p[1]) / d[1];
+	if (abs(tyMin - tMin) < RAY_EPSILON)
+		axis = 1;
 
-	i.obj = this;
-	i.t = tNear;
+	if (d[2] > 0.0)
+		tzMin = (-0.5 - p[2]) / d[2];
+	else
+		tzMin = ( 0.5 - p[2]) / d[2];
+	if (abs(tzMin - tMin) < RAY_EPSILON)
+		axis = 2;
 
 	switch (axis)
 	{
-	case 0: i.N = vec3f(((p[0] < 0.0) ? -1.0f : 1.0f), 0.0f, 0.0f); break;
-	case 1: i.N = vec3f(0.0f, ((p[1] < 0.0) ? -1.0f : 1.0f), 0.0f); break;
-	case 2: i.N = vec3f(0.0f, 0.0f, ((p[2] < 0.0) ? -1.0f : 1.0f)); break;
+	case 0:
+		i.N = vec3f((r.getDirection()[0] < 0.0) ? 1.0 : -1.0, 0.0, 0.0);
+		break;
+	case 1:
+		i.N = vec3f(0.0, (r.getDirection()[1] < 0.0) ? 1.0 : -1.0, 0.0);
+		break;
+	case 2:
+		i.N = vec3f(0.0, 0.0, (r.getDirection()[2] < 0.0) ? 1.0 : -1.0);
+		break;
 	}
-
-	return true;
 }
